2ba7f841fb916360a91908d520cd642bcd2a5e46
piperace <inbox6212@gmail.com>
2026-09-15 21:46:01 +0300

    Release 1.5.23

--- Formula/piperace.rb
+++ Formula/piperace.rb
@@ -2,7 +2,6 @@ class Piperace < Formula
   desc "Expose local ports through a Piperace tunnel"
   homepage "https://piperace.com"
   version "1.5.23"
-  depends_on :macos
 
   on_macos do
     on_arm do
@@ -15,8 +14,19 @@ class Piperace < Formula
     end
   end
 
+  on_linux do
+    on_arm do
+      url "https://piperace-downloads.s3.us-east-1.amazonaws.com/1.5.23/piperace-linux-arm64"
+      sha256 "dd7bc0a5aaaeaa2ecc4b06c85a50854fa69461777f2a79af54a459292eeadf16"
+    end
+    on_intel do
+      url "https://piperace-downloads.s3.us-east-1.amazonaws.com/1.5.23/piperace-linux-amd64"
+      sha256 "9773de071f192a42d3fc82e5f423e3e291197786b00556059ec95779b52cec04"
+    end
+  end
+
   def install
-    binary = Dir["piperace-darwin-*"].first
+    binary = Dir["piperace-*"].first
     bin.install binary => "piperace"
     chmod 0755, bin/"piperace"
   end
